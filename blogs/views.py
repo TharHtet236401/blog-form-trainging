66e2098b1305af67f5af3107dce5428c5643e158
TharHtet236401 <tharhteta3@gmail.com>
2025-05-05 10:33:50 +0700

    django error message appeared

--- blogs/views.py
+++ blogs/views.py
@@ -4,18 +4,26 @@ from .models import BlogPost
 from .forms import BlogPostForm
 
 def blog_list(request):
-    posts = BlogPost.objects.all().order_by('-created_at')
-    return render(request, 'blog/blog_list.html', {'posts': posts})
+    try:
+        posts = BlogPost.objects.all().order_by('-created_at')
+        return render(request, 'blog/blog_list.html', {'posts': posts})
+    except Exception as e:
+        messages.error(request, f"An error occurred while fetching blog posts: {str(e)}")
+        return render(request, 'blog/blog_list.html', {'posts': []})
 
 def blog_create(request):
-    if request.method == 'POST':
-        form = BlogPostForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Blog post created successfully!")
-            return redirect('blog_list')
+    try:
+        if request.method == 'POST':
+            form = BlogPostForm(request.POST)
+            if form.is_valid():
+                form.save()
+                messages.success(request, "Blog post created successfully!")
+                return redirect('blog_list')
+            else:
+                messages.error(request, "Error while creating blog post")
         else:
-            messages.error(request, "Please correct the error below.")
-    else:
-        form = BlogPostForm()
-    return render(request, 'blog/blog_form.html', {'form': form})
+            form = BlogPostForm()
+        return render(request, 'blog/blog_form.html', {'form': form})
+    except Exception as e:
+        messages.error(request, f"An error occurred while creating the blog post: {str(e)}")
+        return render(request, 'blog/blog_form.html', {'form': form})
